2f88cd615f71e6878d34463a53e9e24effa8efd8
isnayg <38523283+is-nan@users.noreply.github.com>
2021-08-22 22:54:12 +0800

    Update index.jsx

--- src/pages/My/index.jsx
+++ src/pages/My/index.jsx
@@ -4,7 +4,7 @@ import './index.scss'
 function My(props) {
   return (
     <View className="My">
-      <Image src="https://www.images.nanbk.com/images/2020/03/12/15652482794301084.jpg"
+      <Image src="https://www.nanbk.com/usr/themes/Akina/images/avatar.png"
       className="Avatar shadow-blur"/>
       <Text className="text-center text-xxl text-black Name">南岸有归</Text>
       <Text className="text-df signature">嗨，我是南岸有归，一名 前端 开发者。</Text>
@@ -12,22 +12,39 @@ function My(props) {
       <Text className="text-center text-lg text-black Name">很高兴认识你</Text>
       <Text className="text-gray text-df text">坐标深圳.</Text>
       <Text className="text-gray text-df text">是一名前端开发者.</Text>
+      <Text className="text-gray text-df text">这个小程序是react博客改版的简单小程序.</Text>
+      <Text className="text-gray text-df text">使用的是Taro开发的.</Text>
       <Text className="text-center text-black Name">熟悉的一些技术</Text>
         <Text className="text-gray text-df text">
-          Javascript、Vue、React、Node.js、Typescript、Mongodb、Mysql、Nginx、小程序开发
+          Javascript、Vue、React、Uni-app、Taro、mpvue、Node.js、Go、Typescript、Mongodb、Mysql、Nginx、小程序开发
         </Text>
       <Text className="text-center text-black Name">还不太熟悉,想了解的技术</Text>
         <Text className="text-gray text-df text">
-          算法、Go、V8
+          算法、V8
         </Text>
       <Text className="text-center text-black Name">写过的项目</Text>
       <Text className="text-gray text-df text">
-        叮叮开课官网、智联物联云平台、智联物联云小程序、react全栈博客、博客小程序
+        叮叮开课官网、智联物联云平台、智联物联云小程序、react全栈博客、博客小程序、会计学堂官网、会计学堂微信小程序、会计学堂百度小程序、i指间App、烟有App
       </Text>
+      <Text className="text-center text-black Name">博客地址(点击下方复制)：</Text>
+        <Text className="text-gray text-df text link" onClick={()=>{
+          Taro.setClipboardData({
+            data: 'https://react.nanbk.com',
+            success: function (res) {
+              Taro.getClipboardData({
+                success: function (res) {
+                  console.log(res.data) // data
+                }
+              })
+            }
+          })
+        }}>
+          react.nanbk.com
+        </Text>
+      <Text className="text-center text-black Name">写过的项目</Text>
       <Text className="text-center text-black Name">联系方式</Text>
       <View className="Item">
-        <Text className="text-df List">邮箱:1149807390@qq.com</Text>
-        <Text className="text-df List">qq:1149807390</Text>
+        <Text className="text-df List">邮箱：isnanyg@gmail.com</Text>
       </View>
     </View>
   )
